ce9b21e415cb751054d601440236c139a1e68ca7
fistkim101 <fistkim101@gmail.com>
2023-03-22 10:37:22 +0900

    lecture07 : value manager

---
+++ lib/presentation/value_manager.dart
@@ -0,0 +1,30 @@
+// 이것도 중앙화 해서 쓰는건 알겠는데.. 예를 들어 app 전체에서 p20 으로 잡은 모든 것을 바꾼다던가 할 때에나 의미가 있지 경험상 그럴 일이 없었다.
+class AppMargin {
+  static const double m8 = 8.0;
+  static const double m12 = 12.0;
+  static const double m14 = 14.0;
+  static const double m16 = 16.0;
+  static const double m18 = 18.0;
+  static const double m20 = 20.0;
+}
+
+class AppPadding {
+  static const double p8 = 8.0;
+  static const double p12 = 12.0;
+  static const double p14 = 14.0;
+  static const double p16 = 16.0;
+  static const double p18 = 18.0;
+  static const double p20 = 20.0;
+}
+
+class AppSize {
+  static const double s8 = 8.0;
+  static const double s12 = 12.0;
+  static const double s14 = 14.0;
+  static const double s16 = 16.0;
+  static const double s18 = 18.0;
+  static const double s20 = 20.0;
+  static const double s28 = 28.0;
+  static const double s60 = 60.0;
+  static const double s65 = 65.0;
+}
